f5b344d0ac038abfd8cb4fe6f75cffaa272dd5fb
kvense <kvenzrefa@gmail.com>
2024-04-12 14:36:22 +0500

    Получение данных о категориях. Тепляков

---
+++ Shop_Тепляков/Data/DataBase/DBCategory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Shop_Тепляков.Data.DataBase
+{
+    public class DBCategory : Interfaces.ICategorys
+    {
+        public IEnumerable<Models.Categorys> AllCategorys
+        {
+            get
+            {
+                List<Models.Categorys> categorys = new List<Models.Categorys>();
+                MySqlConnection MySqlConnection = Common.Connection.MySqlOpen();
+                MySqlDataReader CategorysReader = Common.Connection.MySqlQuery("Select * from pr38.Categorys Order By 'Name';", MySqlConnection);
+                while (CategorysReader.Read())
+                {
+                    categorys.Add(new Models.Categorys()
+                    {
+                        Id = CategorysReader.IsDBNull(0) ? -1 : CategorysReader.GetInt32(0),
+                        Name = CategorysReader.IsDBNull(1) ? null : CategorysReader.GetString(1),
+                        Description = CategorysReader.IsDBNull(2) ? null : CategorysReader.GetString(2),
+                    });
+                }
+                return categorys;
+            }
+        }
+    }
+}
