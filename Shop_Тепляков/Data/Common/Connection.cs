c082b40a6528020b57617bb590ecc8efd85974af
kvense <kvenzrefa@gmail.com>
2024-04-12 14:29:20 +0500

    Разработанное подключение к MySql. Тепляков

---
+++ Shop_Тепляков/Data/Common/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Shop_Тепляков.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=127.0.0.1;port=3308;database=pr38;uid=root;";
+
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection newConnection = new MySqlConnection(ConnectionData);
+            newConnection.Open();
+            return newConnection;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand newCommand = new MySqlCommand(Query, Connection);
+            return newCommand.ExecuteReader();
+        }
+
+        public static void MySqlClose(MySqlConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
